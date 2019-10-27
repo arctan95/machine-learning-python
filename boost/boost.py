2252566a550c68f9ee13552d44d38a487603ed17
arctan95 <tanzq1995@163.com>
2019-10-27 21:31:11 +0800

    finish adaboost

--- boost/boost.py
+++ boost/boost.py
@@ -1,5 +1,5 @@
 from numpy import *
-
+import matplotlib.pyplot as plt
 def loadSimpData():
     datMat = array([[1.,2.1],
                      [2.,1.1],
@@ -74,7 +74,82 @@ def adaBoostTrainDS(dataArr, classLabels, numlt = 40):
         print("total error:", errorRate, "\n")
         # 训练错误为0时，退出循环
         if errorRate == 0.0: break
-    return weakClassArr
+    return weakClassArr,aggClassEst
 # 测试
-classifierArray = adaBoostTrainDS(dataMat, classLabels, 9)
-print(classifierArray)
+# classifierArray = adaBoostTrainDS(dataMat, classLabels, 9)
+# print(classifierArray)
+
+# AdaBoost分类函数
+def adaClassify(dataToClass, classifierArray):
+    dataMatrix = mat(dataToClass)
+    m = shape(dataMatrix)[0]
+    aggClassEst = mat(zeros((m,1)))
+    for i in range(len(classifierArray)):
+        classEst = stumpClassify(dataMatrix, classifierArray[i]['dim'], classifierArray[i]['thresh'], classifierArray[i]['ineq'])
+        aggClassEst += classifierArray[i]['alpha'] * classEst
+        print(aggClassEst)
+    return sign(aggClassEst)
+
+# 测试
+# print(adaClassify([0,0],classifierArray))
+
+# 马疝病的预测
+
+# 自适应数据加载函数
+def loadDataSet(fileName):
+    numFeat = len(open(fileName).readline().split('\t'))
+    dataMat = []; labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat - 1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+
+# 测试
+# dataArr, labelArr = loadDataSet('horseColicTraining2.txt')
+# classifierArray = adaBoostTrainDS(dataArr, labelArr, 10)
+# print(classifierArray)
+# print("-------------------------------------------------")
+# testArr, testLabelArr = loadDataSet('horseColicTest2.txt')
+# prediction10 = adaClassify(testArr, classifierArray)
+# errArr = mat(ones((67,1)))
+# print(errArr[prediction10 != mat(testLabelArr).T].sum())
+
+# ROC曲线的绘制及AUC计算函数
+def plotROC(predStrengths, classLabels):
+    cur = (1.0, 1.0)  # cursor
+    ySum = 0.0  # variable to calculate AUC
+    numPosClas = sum(array(classLabels) == 1.0)
+    yStep = 1 / float(numPosClas);
+    xStep = 1 / float(len(classLabels) - numPosClas)
+    sortedIndicies = predStrengths.argsort()  # get sorted index, it's reverse
+    fig = plt.figure()
+    fig.clf()
+    ax = plt.subplot(111)
+    # loop through all the values, drawing a line segment at each point
+    for index in sortedIndicies.tolist()[0]:
+        if classLabels[index] == 1.0:
+            delX = 0;
+            delY = yStep;
+        else:
+            delX = xStep;
+            delY = 0;
+            ySum += cur[1]
+        # draw line from cur to (cur[0]-delX,cur[1]-delY)
+        ax.plot([cur[0], cur[0] - delX], [cur[1], cur[1] - delY], c='b')
+        cur = (cur[0] - delX, cur[1] - delY)
+    ax.plot([0, 1], [0, 1], 'b--')
+    plt.xlabel('False positive rate'); plt.ylabel('True positive rate')
+    plt.title('ROC curve for AdaBoost horse colic detection system')
+    ax.axis([0, 1, 0, 1])
+    plt.show()
+    print("the Area Under the Curve is: ", ySum * xStep)
+
+# 测试
+dataArr, labelArr = loadDataSet('horseColicTraining2.txt')
+classifierArray, aggClassEst = adaBoostTrainDS(dataArr, labelArr, 10)
+plotROC(aggClassEst.T, labelArr)
