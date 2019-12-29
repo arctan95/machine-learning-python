f2626efff8c67dc61960e0faf4fd9e86e7a8eb50
arctan95 <tanzq1995@163.com>
2019-12-29 22:20:36 +0800

    finish CART

--- CART/regTrees.py
+++ CART/regTrees.py
@@ -28,6 +28,27 @@ def regLeaf(dataSet):
 def regErr(dataSet):
     return var(dataSet[:,-1]) * shape(dataSet)[0]
 
+#模型树的节点生成函数
+def linearSolve(dataSet):
+    m, n = shape(dataSet)
+    X = mat(ones((m, n))); Y = mat(ones((m, 1)))
+    X[:,1:n] = dataSet[:,0:n-1]; Y = dataSet[:,-1]
+    xTx = X.T * X
+    if linalg.det(xTx) == 0.0:
+        raise NameError('This matrix is singular, cannot do inverse,try increasing the second value of ops')
+    ws = xTx.I * (X.T * Y) #xTx.I就是对矩阵xTx求逆矩阵
+    return ws,X,Y
+
+def modelLeaf(dataSet):
+    ws,X,Y = linearSolve(dataSet)
+    return ws
+
+def modelErr(dataSet):
+    ws,X,Y = linearSolve(dataSet)
+    yHat = X * ws
+    return sum(power(Y - yHat,2))
+
+
 def createTree(dataSet, leafType = regLeaf, errType = regErr, ops = (1, 4)):
     feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
     if feat == None: return val
@@ -40,7 +61,7 @@ def createTree(dataSet, leafType = regLeaf, errType = regErr, ops = (1, 4)):
     return retTree
 
 
-
+#回归树切分函数
 def chooseBestSplit(dataSet, leafType = regLeaf, errType = regErr, ops = (1, 4)):
     tolS = ops[0]; tolN = ops[1] # tolS:容许的误差下降值 tolN:切分的最少样本数
     if len(set(dataSet[:,-1].T.tolist()[0])) == 1:
@@ -65,5 +86,97 @@ def chooseBestSplit(dataSet, leafType = regLeaf, errType = regErr, ops = (1, 4))
     return bestIndex, bestValue
 
 #测试
-myDat = mat(loadDataSet('ex00.txt'))
-print(createTree(myDat))
+# myDat = mat(loadDataSet('ex00.txt'))
+# print(createTree(myDat))
+
+# myMat = mat(loadDataSet('ex0.txt'))
+# print(createTree(myMat))
+
+def isTree(obj):
+    return (type(obj).__name__ == 'dict')
+
+def getMean(tree):
+    if isTree(tree['right']):tree['right'] = getMean(tree['right'])
+    if isTree(tree['left']):tree['left'] = getMean(tree['left'])
+    return (tree['left'] + tree['right']) / 2.0
+
+#回归树剪枝函数
+def prune(tree, testData):
+    if shape(testData)[0] == 0:return getMean(tree)
+    if (isTree(tree['right']) or isTree(tree['left'])):
+        lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+    if isTree(tree['left']):tree['left'] = prune(tree['left'], lSet)
+    if isTree(tree['right']):tree['right'] = prune(tree['right'], rSet)
+    if not isTree(tree['left']) and not isTree(tree['right']):
+        lSet, rSet = binSplitDataSet(testData,tree['spInd'],tree['spVal'])
+        errorNoMerge = sum(power(lSet[:,-1] - tree['left'],2)) + sum(power(rSet[:,-1] - tree['right'],2))
+        treeMean = (tree['left'] + tree['right']) / 2.0
+        errorMerge = sum(power(testData[:,-1] - treeMean, 2))
+        if errorMerge < errorNoMerge:
+            print("merging")
+            return treeMean
+        else:
+            return tree
+    else:return tree
+
+# 测试
+# myMat2 = mat(loadDataSet('ex2.txt'))
+# myTree = createTree(myMat2, ops= (0,1))
+# print(myTree)
+# myMat2Test = mat(loadDataSet('ex2test.txt'))
+# print(prune(myTree,myMat2Test))
+
+#测试
+# myMat2 = mat(loadDataSet('exp2.txt'))
+# print(createTree(myMat2, modelLeaf,modelErr,(1,10)))
+
+#树回归与标准回归的比较
+
+#用树回归进行预测
+def regTreeEval(model, inDat):
+    return float(model)
+
+def modelTreeEval(model, inDat):
+    n = shape(inDat)[1]
+    X = mat(ones((1, n + 1)))
+    X[:,1:n+1] = inDat
+    return float(X * model)
+
+def treeForeCast(tree, inData, modelEval = regTreeEval):
+    if not isTree(tree):return modelEval(tree, inData)
+    if inData[tree['spInd']] > tree['spVal']:
+        if isTree(tree['left']):
+            return treeForeCast(tree['left'], inData, modelEval)
+        else:
+            return modelEval(tree['left'], inData)
+    else:
+        if isTree(tree['right']):
+            return treeForeCast(tree['right'], inData, modelEval)
+        else:
+            return modelEval(tree['right'], inData)
+
+def createForeCast(tree, testData, modelEval = regTreeEval):
+    m = len(testData)
+    yHat = mat(zeros((m, 1)))
+    for i in range(m):
+        yHat[i, 0] = treeForeCast(tree, mat(testData[i]), modelEval)
+    return yHat
+
+#测试
+trainMat = mat(loadDataSet('bikeSpeedVsIq_train.txt'))
+testMat = mat(loadDataSet('bikeSpeedVsIq_test.txt'))
+# myTree = createTree(trainMat, ops = (1,20))
+# yHat = createForeCast(myTree, testMat[:,0])
+# print(corrcoef(yHat, testMat[:,1], rowvar = 0)[0, 1])
+
+myTree = createTree(trainMat, modelLeaf, modelErr, (1,20))
+yHat = createForeCast(myTree, testMat[:,0], modelTreeEval)
+print(corrcoef(yHat, testMat[:,1], rowvar = 0)[0, 1])
+
+ws, X, Y = linearSolve(trainMat)
+print(ws)
+
+for i in range(shape(testMat)[0]):
+    yHat[i] = testMat[i,0] * ws[1,0] + ws[0,0]
+
+print(corrcoef(yHat, testMat[:,1],rowvar=0)[0,1])
